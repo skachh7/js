442f2898f216130f2833c0f0bcebeea666d78005
Samir Kachhapati <samir.kachhapatiofficial@gmail.com>
2023-09-17 18:21:54 +1000

    Added ways to get from input value

--- bmi.js
+++ bmi.js
@@ -1,5 +1,7 @@
-function calculateBmi(height, weight){
-    bmi=weight/(height*height);
-    document.getElementById()
+function calculateBmi(){
+    let weight= document.getElementById("input-number-weight").value;
+    let height= document.getElementById("input-number-height").value;
+    let bmi=weight/(height*height);
+    document.getElementById("span-result").innerHTML=bmi;
 
 }
