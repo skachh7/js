64a33ef46bbecc0affef3abce68eefee313d9639
Samir Kachhapati <samir.kachhapatiofficial@gmail.com>
2023-09-17 18:36:13 +1000

    Added event and comment

--- bmi.js
+++ bmi.js
@@ -3,5 +3,13 @@ function calculateBmi(){
     let height= document.getElementById("input-number-height").value;//WIll take value from input
     let bmi=weight/(height*height);//Formula
     document.getElementById("span-result").innerHTML=bmi;//Provide results to span id in webpage.
-
 }
+//When we cahnge value in weight or height input then we call an event and update the value in it.
+function handleWeightInput(event){
+    document.getElementById("input-number-weight").value=event.target.value;
+}
+
+function handleHeightInput(event){
+    document.getElementById("input-number-height").value=event.target.value;
+}
+
