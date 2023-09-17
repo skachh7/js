daa42c77bd35911f26bfb99c6451a65290e34e41
Samir Kachhapati <samir.kachhapatiofficial@gmail.com>
2023-09-17 18:27:51 +1000

    Added comment

--- bmi.js
+++ bmi.js
@@ -1,7 +1,7 @@
 function calculateBmi(){
-    let weight= document.getElementById("input-number-weight").value;
-    let height= document.getElementById("input-number-height").value;
-    let bmi=weight/(height*height);
-    document.getElementById("span-result").innerHTML=bmi;
+    let weight= document.getElementById("input-number-weight").value;//Will take value from input
+    let height= document.getElementById("input-number-height").value;//WIll take value from input
+    let bmi=weight/(height*height);//Formula
+    document.getElementById("span-result").innerHTML=bmi;//Provide results to span id in webpage.
 
 }
