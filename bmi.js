87187b3d30de7850fcfb1ee863ac75f241d6b65a
Samir Kachhapati <samir.kachhapatiofficial@gmail.com>
2023-09-17 18:50:57 +1000

    Added condition

--- bmi.js
+++ bmi.js
@@ -3,7 +3,17 @@ function calculateBmi(){
     let height= document.getElementById("input-number-height").value;//WIll take value from input
     let bmi=weight/(height*height);//Formula
     document.getElementById("span-result").innerHTML=bmi;//Provide results to span id in webpage.
-}
+    if (bmi<18.5){
+        document.getElementById("span-result").style.color = "red";
+        }
+        else if (bmi>=18.5 && bmi<=29.5 ){
+        document.getElementById("span-result").style.color = "green";
+        }
+        else{
+        document.getElementById("span-result").style.color = "orange";
+        
+        }
+        
 /* //When we cahnge value in weight or height input then we call an event and update the value in it.
 function handleWeightInput(event){
     document.getElementById("input-number-weight").value=event.target.value;
@@ -15,5 +25,6 @@ function handleHeightInput(event){
  */
 
 function handleInputChange(event){
-    document.getElementByNames("event.target.name").value=event.target.value;
+    document.getElementsByName("event.target.name").value=event.target.value;
+}
 }
